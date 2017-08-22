54735ef2a8fb58ccb10066188d3fbf98c3f00226
Juan Gómez <juanantonio.g@lazarus-staff.com>
2017-08-22 14:04:46 +0100

    Initial comit

---
+++ app.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+    <Greeting message='Hello World!!' />,
+    document.getElementById('app')
+);
---
+++ components/greeting.js
@@ -0,0 +1,5 @@
+class Greeting extends React.Component {
+    render() {
+        return (<p>{this.props.message}</p>);
+    }
+}
